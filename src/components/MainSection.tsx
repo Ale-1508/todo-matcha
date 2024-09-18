5fce8b53547b1f2d121d1c9accc3322d6e0075ee
Alessandro <alessandromasiero158@libero.it>
2024-09-18 22:39:01 +0200

    Added responsive behavor to main section

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -2,38 +2,47 @@ import { Box, Text, Flex } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import ToDo from "./ToDo/ToDo";
 import colors from "../context/colors";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 const boxStyle = {
   borderRadius:"24px",
   overflow:"hidden",
 }
 
-const MainSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground={true}
-    backgroundColor={colors.background}
-  >
-    <Flex 
-      gap="10px" 
-      padding="64px 16px 16px 16px"
-      height="100vh"
-      width="100vw"
-      overflow="auto"
+const MainSection = () => {
+  const { height, width } = useWindowDimensions();
+  
+  return(
+    <FullScreenSection
+      justifyContent="center"
+      alignItems="center"
+      isDarkBackground={true}
+      backgroundColor={colors.background}
     >
-      <ToDo boxStyle={boxStyle}/>
-      <Box
-        { ...boxStyle }
-        flex="2"
-        background="transparent"
-        justifyContent="center"
-        alignContent="center"
-        >
-        <Text>Box 1</Text>
-      </Box>
-    </Flex>
-  </FullScreenSection>
-);
+      <Flex 
+        gap="10px" 
+        padding="64px 16px 16px 16px"
+        height="100vh"
+        width="100vw"
+        overflow="auto"
+      >
+        <ToDo boxStyle={boxStyle}/>
+        { width > 950 
+          ? <Box
+            { ...boxStyle }
+            flex={width > 1400 ? "2" : "1" }
+            background="transparent"
+            justifyContent="center"
+            alignContent="center"
+            >
+            <Text>Box 1</Text>
+          </Box>
+          : null
+        }
+        
+      </Flex>
+    </FullScreenSection>
+  );
+};
 
 export default MainSection;
